d819dae69470e699bf3cb786c6841a2eac891263
Anshul-ydv <anshulyadav802@gmail.com>
2025-06-07 00:07:03 +0530

    Initial commit: Student Registration Web App

---
+++ StudentRegistrationWeb/Default.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StudentRegistrationWeb.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-bottom: 1rem">
+            <table>
+                <tr>
+                    <td>Student Name</td>
+                    <td>
+                        <asp:TextBox ID="txtStudentName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>
+                        <asp:DropDownList ID="ddlGender" runat="server">
+                            <asp:ListItem>Male</asp:ListItem>
+                            <asp:ListItem>Female</asp:ListItem>
+                            <asp:ListItem>Others</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Mobile No</td>
+                    <td><asp:TextBox ID="txtMobileNo" runat="server" TextMode="Phone"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><asp:TextBox ID="txtEmail" runat="server" TextMode="Email"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" /></td>
+                </tr>
+                <tr>
+                    <td>Message:</td>
+                    <td><asp:Label ID="lblMessage" runat="server"></asp:Label></td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <asp:GridView ID="grdStudent" runat="server" AutoGenerateColumns="false" CellPadding="4" OnRowEditing="grdStudent_RowEditing" OnRowCancelingEdit="grdStudent_RowCancelingEdit" OnRowUpdating="grdStudent_RowUpdating" OnRowDeleting="grdStudent_RowDeleting" >
+                <Columns>
+                    <asp:TemplateField HeaderText="Student ID">
+                        <ItemTemplate>
+                            <asp:Label ID="lblStudentID" runat="server" Text='<%# Eval("StudentId") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Student Name">
+                        <ItemTemplate>
+                            <asp:Label ID="lblStudentName" runat="server" Text='<%# Eval("StudentName") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtStudentName" runat="server" Text='<%# Eval("StudentName") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Gender">
+                        <ItemTemplate>
+                            <asp:Label ID="lblGender" runat="server" Text='<%# Eval("Gender") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:DropDownList ID="drpGender" runat="server" >
+                                <asp:ListItem>Male</asp:ListItem>
+                                <asp:ListItem>Female</asp:ListItem>
+                                <asp:ListItem>Others</asp:ListItem>
+                            </asp:DropDownList>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Mobile No">
+                        <ItemTemplate>
+                            <asp:Label ID="lblMobileNo" runat="server" Text='<%# Eval("Mobile") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtMobile" runat="server" Text='<%# Eval("Mobile") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="E-Mail">
+                        <ItemTemplate>
+                            <asp:Label ID="lblEmail" runat="server" Text='<%# Eval("Email") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEmail" runat="server" Text='<%# Eval("Email") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Actions">
+                        <ItemTemplate>
+                            <asp:Button ID="btnEdit" runat="server" CommandName="Edit" Text="Edit" />
+                            <asp:Button ID="btnDelete" runat="server" CommandName="Delete" Text="Delete" />
+                         </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:Button ID="btnUpdate" runat="server" CommandName="Update" Text="Update" />
+                            <asp:Button  ID="btnCancel" runat="server" CommandName="Cancel" Text="Cancel" />
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudentRegistrationWeb/Default.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentRegistrationWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"server=(localdb)\localDB; database=regisdb; Integrated Security=True; MultipleActiveResultSets=True; ");
+
+                string query = "insert into student(StudentName, Gender, Mobile, Email) values (@name, @gender, @mobile, @email)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                //SqlCommand cmd = con.CreateCommand();
+                //cmd.CommandText = query;
+
+                cmd.Parameters.AddWithValue("@name", txtStudentName.Text);
+                cmd.Parameters.AddWithValue("@gender", ddlGender.SelectedValue);
+                cmd.Parameters.AddWithValue("@mobile", txtMobileNo.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+
+                con.Open();
+
+                int i = cmd.ExecuteNonQuery();
+
+                if (i != 0)
+                {
+                    lblMessage.Text = "Student registered Successfully.";
+                    BindGridView();
+                } else
+                {
+                    lblMessage.Text = "Student registration failed.";
+                }
+
+                con.Close();
+            } 
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        private void BindGridView()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"server=(localdb)\localDB; database=regisdb; Integrated Security=True; MultipleActiveResultSets=True; ");
+
+                string query = "select StudentId, StudentName, Gender, Mobile, Email from student";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                //SqlCommand cmd = con.CreateCommand();
+                //cmd.CommandText = query;
+
+
+                con.Open();
+                DataTable dt = new DataTable();
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                dt.Load(sdr);
+
+                grdStudent.DataSource = dt;
+                grdStudent.DataBind();
+                
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void grdStudent_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            int idx = e.NewEditIndex;
+            grdStudent.EditIndex = idx;
+            BindGridView();
+        }
+
+        protected void grdStudent_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdStudent.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void grdStudent_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = grdStudent.Rows[e.RowIndex];
+                int studentid = Convert.ToInt32(((Label)row.FindControl("lblStudentID")).Text);
+
+                string studentName = ((TextBox)row.FindControl("txtStudentName")).Text;
+                string gender = ((DropDownList)row.FindControl("drpGender")).SelectedValue;
+                string mobile = ((TextBox)row.FindControl("txtMobile")).Text;
+                string email = ((TextBox)row.FindControl("txtEmail")).Text;
+
+                SqlConnection con = new SqlConnection(@"server=(localdb)\localDB; database=regisdb; Integrated Security=True; MultipleActiveResultSets=True; ");
+
+                string query = "update student set StudentName=@studentName, gender=@gender, mobile=@mobile, email=@email where studentid=@studentid;";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studentid", studentid);
+                cmd.Parameters.AddWithValue("@studentName", studentName);
+                cmd.Parameters.AddWithValue("@gender", gender);
+                cmd.Parameters.AddWithValue("@mobile", mobile);
+                cmd.Parameters.AddWithValue("@email", email);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                grdStudent.EditIndex = -1;
+
+                BindGridView();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void grdStudent_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = grdStudent.Rows[e.RowIndex];
+                int studentid = Convert.ToInt32(((Label)row.FindControl("lblStudentID")).Text);
+
+
+                SqlConnection con = new SqlConnection(@"server=(localdb)\localDB; database=regisdb; Integrated Security=True; MultipleActiveResultSets=True; ");
+
+                string query = "delete from student where studentid=@studentid;";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studentid", studentid);
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                BindGridView();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
